780a5a1aa82fb24ef826b93d3ece2f57801575a3
park <qkrfowns45@naver.com>
2020-12-22 16:10:09 +0900

    웹 프로그래밍 기말 텀 프로젝트

---
+++ calender.js
@@ -0,0 +1,12 @@
+const now = new Date();
+
+const today = {
+    year : now.getFullYear(),
+    date : now.getDate(),
+    month : now.toLocaleString('en-US', {month: 'short'}),
+    day: now.toLocaleString('en-Us', {weekday: 'short'})
+}
+
+for(let key in today){
+    document.getElementById(key).textContent = today[key];
+}
